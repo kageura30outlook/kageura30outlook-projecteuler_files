bb7e61c7bfb0cedf3ad3f2ec8ed54e65f06583f1
Kageura <kageura30@outlook.com>
2024-12-10 22:00:41 +0900

    Delete q31_othercoinsums.py

--- q31_othercoinsums.py
+++
@@ -1,8 +0,0 @@
-price = 200
-coin = [200,100,50,20,10,5,2,1]
-
-for i in range (8):#coin no kazu wo kokoni ireru (koko)
-    count = price / coin[i]+count
-    price = price % coin[i]
-
-print(count)
