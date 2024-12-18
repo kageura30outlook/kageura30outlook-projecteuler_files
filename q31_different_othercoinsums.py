44b524c3d639dd6d2503f4db4e5db610fc75f876
Kageura <kageura30@outlook.com>
2024-12-18 14:39:26 +0900

    Update q31_different_othercoinsums.py

--- q31_different_othercoinsums.py
+++ q31_different_othercoinsums.py
@@ -1,7 +1,7 @@
 price = 200
 coin = [200,100,50,20,10,5,2,1]
 
-for i in range (8):#coin no kazu wo kokoni ireru (koko)
+for i in range (8):#Put number of coins here
     count = price / coin[i]+count
     price = price % coin[i]
 
